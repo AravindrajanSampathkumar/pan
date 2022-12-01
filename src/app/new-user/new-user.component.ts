749a68b4271c90dd209c8dc899f5cfc0da81688d
Aravindrajan_Sampath <Aravindrajan_Sampath@Dell.com>
2022-12-01 20:47:07 +0530

    Form validation

--- src/app/new-user/new-user.component.ts
+++ src/app/new-user/new-user.component.ts
@@ -11,35 +11,108 @@ import { AuthService } from '../service/auth.service';
 export class NewUserComponent implements OnInit, OnDestroy {
   createNewUserForm: any;
   registerSubmitted = false;
+  gender = [
+    { name: 'Male', value: 'male' },
+    { name: 'Female', value: 'female' },
+    { name: 'Other', value: 'other' }];
+  knowOthers = [
+    { name: 'Yes', value: 'yes' },
+    { name: 'No', value: 'no' },
+  ];
+  singleParents = [
+    { name: 'Yes', value: 'yes' },
+    { name: 'No', value: 'no' },
+  ];
+  panDetails = [
+    { name: 'Yes', value: 'yes' },
+    { name: 'No', value: 'no' },
+  ];
+  sourceOfIncomeData = [
+    { name: 'Yes', value: 'yes' },
+    { name: 'No', value: 'no' },
+  ];
+  titles = [
+    { name: 'Shri', value: 'shri' },
+    { name: 'Smt', value: 'smt' },
+    { name: 'Kumari', value: 'kumari' }
+  ]
   constructor(public authService: AuthService, private router: Router) { }
 
   ngOnInit() {
     this.createNewUserForm = new FormGroup({
-      fname: new FormControl('', [Validators.required]),
-      lname: new FormControl('', [Validators.required]),
-      aadhar: new FormControl('', [Validators.required]),
-      address: new FormControl('', [Validators.required]),
-      phoneNumber: new FormControl('', [Validators.required, Validators.min(1000000000), Validators.pattern(("[6-9]\\d{9}"))]),
-      email: new FormControl('', [Validators.required, Validators.pattern(("[a-z0-9!#$%&'*+/=?^_`{|}~.-]+@[a-z0-9-]+(\.[a-z0-9-]+)*"))]),
-      password: new FormControl('', [Validators.required]),
-      confirmPassword: new FormControl('', [Validators.required]),// (control) => this.passwordMatchValidator(control, 'confirmPassword')
+      allication49A: new FormControl('', [Validators.required]),
+      title: new FormControl('', [Validators.required]),
+      fName: new FormControl('', [Validators.required]),
+      mName: new FormControl(''),
+      lName: new FormControl(''),
       gender: new FormControl('', [Validators.required]),
       dob: new FormControl('', [Validators.required]),
+      namePan: new FormControl('', [Validators.required]),
+      consent: new FormControl('', [Validators.required]),
+      kOther: new FormControl('', [Validators.required]),
+      oFname: new FormControl(''),
+      oMname: new FormControl(''),
+      oLname: new FormControl(''),
+      category: new FormControl('', [Validators.required]),
+      aadharName: new FormControl('', [Validators.required]),
+      aadharNum: new FormControl('', [Validators.required]),
+      sinPatent: new FormControl('', [Validators.required]),
+      fFname: new FormControl('', [Validators.required]),
+      fMname: new FormControl(''),
+      fLname: new FormControl(''),
+      mFname: new FormControl('', [Validators.required]),
+      mMname: new FormControl(''),
+      mLname: new FormControl(''),
+      pParentName: new FormControl('', [Validators.required]),
+      pPanReq: new FormControl('', [Validators.required]),
+      applicationModeK: new FormControl('', [Validators.required]),
+      phoneNumber: new FormControl('', [Validators.required, Validators.min(1000000000), Validators.pattern(("[6-9]\\d{9}"))]),
+      email: new FormControl('', [Validators.required, Validators.pattern(("[a-z0-9!#$%&'*+/=?^_`{|}~.-]+@[a-z0-9-]+(\.[a-z0-9-]+)*"))]),
+      place: new FormControl('', [Validators.required]),
+      stdCode: new FormControl('', [Validators.required]),
+      isdCode: new FormControl('', [Validators.required]),
+      sourOfIncome: new FormControl('', [Validators.required]),
+      salary: new FormControl('', [Validators.required]),
+      othrSource: new FormControl(''),
+      bussinessProof: new FormControl(''),
+      bussinessProofCode: new FormControl(''),
+      noIncome: new FormControl(''),
+      housePro: new FormControl(''),
+      capitalGain: new FormControl(''),
+      addressForComm: new FormControl('', [Validators.required]),
+      residentAddress: new FormControl('', [Validators.required]),
+      flatNo: new FormControl(''),
+      streetName: new FormControl(''),
+      roadNo: new FormControl(''),
+      area: new FormControl(''),
+      town: new FormControl('', [Validators.required]),
+      state: new FormControl('', [Validators.required]),
+      country: new FormControl('', [Validators.required])
     });
     this.authService.userIsLoggedIn.next(true);
   }
 
-  register() {
-    if (this.createNewUserForm.controls['confirmPassword'].value != this.createNewUserForm.controls['password'].value) {
-      this.createNewUserForm.controls['confirmPassword'].setErrors({ 'mismatch': true });
-    } else {
-      this.createNewUserForm.controls['confirmPassword'].setErrors(null)
-    }
+  createUser() {
     this.registerSubmitted = true;
-    console.log('loginForm===============', this.createNewUserForm.value);
-    console.log('registered')
+    let valid = this.checkValidForm(this.createNewUserForm.value)
+    if (valid) {
+      this.router.navigate(['/dashboard']);
+    } else {
+      return;
+    }
   }
 
+  checkValidForm(data: any) {
+    if (data.country && data.state && data.town && data.residentAddress && data.addressForComm && data.salary && data.sourOfIncome
+      && data.isdCode && data.category && data.kOther && data.consent && data.namePan && data.title && data.allication49A &&
+      data.stdCode && data.place && data.email && data.phoneNumber && data.applicationModeK && data.pPanReq && data.pParentName && data.mFname &&
+      data.fFname && data.aadharNum && data.sinPatent && data.aadharName && data.dob && data.gender && data.fName) {
+      console.log('loginForm===============', this.createNewUserForm);
+      return true;
+    } else {
+      return false
+    }
+  }
   navigateBack() {
     this.router.navigate(['/dashboard']);
   }
